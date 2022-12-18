8136c423e67ac780ad218e383c3c48e18040d918
DianaRyz <dianaryzhenkova1@gmail.com>
2022-12-18 18:58:11 +0400

    Create new column with class mark (task 3)

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import pandas as pd
 
 
+def mark_class(row) -> int:
+    mark = 1
+    if row["class"] == "tiger":
+        mark = 0
+    return mark
+
+
 if __name__ == "__main__":
     df = pd.read_csv("dataset.csv", sep=";")
-    df = df.rename(columns={'Absolute way': 'absolute_way'})
-    df = df.rename(columns={'Class': 'class'})
+    df = df.rename(columns={"Absolute way": "absolute_way"})
+    df = df.rename(columns={"Class": "class"})
     df = df.drop(["Relative way"], axis=1)
+
+    df["mark"] = df.apply(mark_class, axis=1)
 
     print(df)
