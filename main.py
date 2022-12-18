a00eba200f7486ec850db6ebe15739111aa6e2ad
DianaRyz <dianaryzhenkova1@gmail.com>
2022-12-18 19:14:35 +0400

    Create new columns with width, height and depth images (task 4)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import cv2
 
 
 def mark_class(row) -> int:
@@ -8,12 +9,34 @@ def mark_class(row) -> int:
     return mark
 
 
+def width(row) -> int:
+    image = cv2.imread(row["absolute way"])
+    w = image.shape[1]
+    return w
+
+
+def height(row) -> int:
+    image = cv2.imread(row["absolute way"])
+    h = image.shape[0]
+    return h
+
+
+def depth(row) -> int:
+    image = cv2.imread(row["absolute way"])
+    d = image.shape[2]
+    return d
+
+
 if __name__ == "__main__":
     df = pd.read_csv("dataset.csv", sep=";")
-    df = df.rename(columns={"Absolute way": "absolute_way"})
+    df = df.rename(columns={"Absolute way": "absolute way"})
     df = df.rename(columns={"Class": "class"})
     df = df.drop(["Relative way"], axis=1)
 
     df["mark"] = df.apply(mark_class, axis=1)
+
+    df["height"] = df.apply(height, axis=1)
+    df["width"] = df.apply(width, axis=1)
+    df["depth"] = df.apply(depth, axis=1)
 
     print(df)
