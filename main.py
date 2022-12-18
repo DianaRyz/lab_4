cd8a1ae7ddcbf0236814095b1131e43600721328
DianaRyz <dianaryzhenkova1@gmail.com>
2022-12-18 19:43:36 +0400

    Create function for filtering by class mark (task 6)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2
 
 
 def mark_class(row) -> int:
+    """Mark for class"""
     mark = 1
     if row["class"] == "tiger":
         mark = 0
@@ -10,21 +11,30 @@ def mark_class(row) -> int:
 
 
 def width(row) -> int:
+    """Calculation width"""
     image = cv2.imread(row["absolute way"])
     w = image.shape[1]
     return w
 
 
 def height(row) -> int:
+    """Calculation height"""
     image = cv2.imread(row["absolute way"])
     h = image.shape[0]
     return h
 
 
 def depth(row) -> int:
+    """Calculation depth"""
     image = cv2.imread(row["absolute way"])
     d = image.shape[2]
     return d
+
+
+def sort_mark(dframe, mark: int):
+    """Filtering by class mark"""
+    dframe = df[dframe.mark == mark]
+    return dframe
 
 
 if __name__ == "__main__":
@@ -38,8 +48,6 @@ if __name__ == "__main__":
     df["height"] = df.apply(height, axis=1)
     df["width"] = df.apply(width, axis=1)
     df["depth"] = df.apply(depth, axis=1)
+    df.describe()
 
-    """statistical data"""
-    print(df.describe())
-
-
+    print(sort_mark(df, 0))
